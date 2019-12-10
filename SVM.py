fcc6ddaa90c7e0292b00b60aae5efc7d0e21be2a
tro16 <tim.roethig@yahoo.de>
2019-12-10 03:52:09 +0100

    grid search

--- SVM.py
+++ SVM.py
@@ -21,16 +21,38 @@ def svm(word_frequency, C, kernel, poly_degree):
     return acc
 
 def grid_search_word_frequency():
-    word_frequencys = [100, 200, 300, 400, 500, 1000, 1500]
+    word_frequencys = [250, 500, 750, 1000, 1250, 1500]
     score = []
     for word_frequency in word_frequencys:
         accuracys = []
         for i in range(3):
-            accuracy = svm(word_frequency, 1, 'linear', 3)
+            accuracy = svm(word_frequency, 3, 'rbf', 3)
             accuracys.append(accuracy)
         mean_accuracy = mean(accuracys)
         score.append([word_frequency, mean_accuracy])
 
     print(score)
 
+def grid_search_hyperparameters():
+    Cs = [3, 4, 5, 6, 7, 8, 9, 10]
+    kernels = ['linear', 'rbf']
+    poly_degrees = [2, 3, 4, 5, 6]
+    score = []
+    for C in Cs:
+        for kernel in kernels:
+            if kernel == 'poly':
+                for poly_degree in poly_degrees:
+                    print([C, kernel, poly_degree])
+                    accuracy = svm(word_frequency=1000, C=C, kernel=kernel, poly_degree=poly_degree)
+                    score.append([accuracy, C, kernel, poly_degree])
+            else:
+                print([C, kernel, None])
+                accuracy = svm(word_frequency=1000, C=C, kernel=kernel, poly_degree=3)
+                score.append([accuracy, C, kernel, None])
+
+    print(score)
+
+
 grid_search_word_frequency()
+
+#svm(word_frequency=1000, C=3, kernel='rbf', poly_degree=3)
